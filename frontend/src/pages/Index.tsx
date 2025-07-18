48690fdc9aa340da0d2b2cbc617fa05d310d929b
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-07-18 12:12:19 +0200

    Layout

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,22 +1,15 @@
+import Layout from '@/components/Layout';
 import Navbar from '@/components/Navbar';
-import BackgroundAnimation from '@/components/BackgroundAnimation';
-import FloatingElements from '@/components/FloatingElements';
 import HeroSection from '@/components/HeroSection';
 import FeatureBlocks from '@/components/FeatureBlocks';
 import ValueProposition from '@/components/ValueProposition';
 import CallToAction from '@/components/CallToAction';
-import Footer from '@/components/Footer';
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background relative overflow-x-hidden">
-      {/* Background Effects */}
-      <BackgroundAnimation />
-      <FloatingElements />
-      
+    <Layout>
       {/* Navigation */}
       <Navbar />
-      
       {/* Main Content */}
       <main>
         <HeroSection />
@@ -24,10 +17,7 @@ const Index = () => {
         <ValueProposition />
         <CallToAction />
       </main>
-      
-      {/* Footer */}
-      <Footer />
-    </div>
+    </Layout>
   );
 };
 
